1d7db27ead81ce3186b6d4ea653700d9d50bea58
Sandeep <sandeep@opstree.com>
2022-10-30 20:24:04 +0530

    Added method for validating if a tag already exists

--- calculateDORAMetrics.sh
+++ calculateDORAMetrics.sh
@@ -6,6 +6,17 @@ function logInfoMessage() {
     echo -e "[$CURRENT_DATE] "$COLOR_START$GREEN[INFO]$COLOR_END" $MESSAGE"
 }
 
+function tagExists() {
+    tagName=$1
+    tagListName=`git tag -l $tagName`
+    if [ "$tagListName" = "$tagName" ]; then
+        tagExists=0
+    else
+        tagExists=1
+    fi
+    echo $tagExists
+}
+
 function createReleaseTag() {
     releaseName=$1
     releaseTag="$releaseName#release"
